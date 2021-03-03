4d2201155abcbd962a6f6b02dbc4f099675ac332
Chad-Mowbray <mowbray.chad@gmail.com>
2021-03-03 13:47:29 -0600

    add fetch

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("amazing things")
+
+
+let interactive = document.querySelector("#interactive")
+console.log(interactive)
+
+interactive.addEventListener('click', function(e) {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(res => res.json())
+  .then(res => interactive.innerHTML = res)
+  .catch(e => interactive.innerHTML = e)
+})
